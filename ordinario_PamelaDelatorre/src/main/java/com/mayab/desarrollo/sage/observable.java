4b35a5e25cc12b1dcc10e021c72e4c6d315b8d07
Pameladlt <pamela_delatorrej@outlook.com>
2019-05-24 13:36:23 -0500

    Update observable.java

--- ordinario_PamelaDelatorre/src/main/java/com/mayab/desarrollo/sage/observable.java
+++ ordinario_PamelaDelatorre/src/main/java/com/mayab/desarrollo/sage/observable.java
@@ -6,9 +6,9 @@ public interface observable {
     
     public String getEstado();
     
-    public void remove(gerente pasajero);
+    public void remove(gerente gerente);
     
-    public void add(gerente pasajero);
+    public void add(gerente gerente);
     
     public void notifyO(String venta);
     
